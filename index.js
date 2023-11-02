68977f616fca7f42e1cf367d6a9ff9cd05ab2e50
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-11-02 10:50:07 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customName = 'bob'
+
+function upperCaseCustomerName() {
+    return customName = customName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'bob'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob'
+}
